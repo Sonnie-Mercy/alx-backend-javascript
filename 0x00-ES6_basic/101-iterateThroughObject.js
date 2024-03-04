9a06d60e24fc824f98a2342ef2a77785f61f6ee7
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-03-04 20:10:13 +0000

    ECMA

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const emps = [];
+  for (const emp of reportWithIterator) {
+    emps.push(emp);
+  }
+  return emps.join(' | ');
+}
