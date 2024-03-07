671a49bdaa4e4669c236f2859fb1e47e9194027f
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-03-07 12:45:44 +0000

    classes JS intro

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get studnts() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new Error('students must be an array of strings');
+    }
+    this._students = students;
+  }
+}
