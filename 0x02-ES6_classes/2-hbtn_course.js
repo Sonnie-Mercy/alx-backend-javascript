570f1b418ce9c3e73e58e96c92b986f9a8f5f496
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-03-08 12:58:29 +0000

    task 2 correction

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,6 +3,15 @@ export default class HolbertonCourse {
     this._name = name;
     this._length = length;
     this._students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array of strings');
+    }
   }
 
   // name
