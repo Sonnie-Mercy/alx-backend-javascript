d4c761422a2f8215eb8459ede26b842cd1339e2f
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-03-12 14:38:45 +0000

    taask 10

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,14 +2,14 @@ export default function cleanSet(set, startString) {
   let result = '';
 
   if (startString === '') {
-    result = '';
-  } else {
-    set.forEach((value) => {
-      if (value.startsWith(startString)) {
-        result += `${value.substring(startString.length)}-`;
-      }
-    });
+    return '';
   }
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += `${value.substring(startString.length)}-`;
+    }
+  });
+
   result = result.slice(0, -1);
 
   return result;
