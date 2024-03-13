0ce8abb69115906fcae881d5495f3b5afd007229
Mercy  Ndirangu <105234288+Sonnie-Mercy@users.noreply.github.com>
2024-03-13 15:07:10 +0300

    task 0 - intro to typescript

--- 0x04-TypeScript/task_0/main.ts
+++ 0x04-TypeScript/task_0/main.ts
@@ -1,2 +1,41 @@
 export interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+const student1: Student = {
+   firstName: "Mercy",
+   lastName: "Sonnie",
+   age: 30,
+   location: "Gilgil, Kenya",
+};
+const student2: Student ={
+    firstName: "Tara",
+    lastName: "Acharya",
+    age: 28,
+    location: "Bodhgaya, India"
+};
+const studentsList: Array<Student> = [student1, student2,];
+function renderTable(students: Student[]) {
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
 
+    students.forEach(student => {
+        const row = document.createElement('tr');
+        const firstNameCell = document.createElement('td');
+        const locationCell = document.createElement('td');
+
+        firstNameCell.textContent = student.firstName;
+        locationCell.textContent = student.location;
+
+        row.appendChild(firstNameCell);
+        row.appendChild(locationCell);
+
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+}
+
+renderTable(studentsList);
