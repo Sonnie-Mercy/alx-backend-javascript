469456e13c0b890fa8f8e8db93987955dffbda85
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-05-28 17:09:31 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([userPromise, photoPromise]) => {
-      const userStatus = userPromise ? 'fulfilled' : 'rejected';
-      const photoStatus = photoPromise ? 'fulfilled' : 'rejected';
-
-      return [
-        { status: userStatus, value: userPromise || 'Error' },
-        { status: photoStatus, value: photoPromise || 'Error' },
-      ];
-    })
-    // .catch((error) => { console.error('Error:', error.message);
-    //  return [
-    //    { status: 'rejected', value: error.message },
-    //    { status: 'rejected', value: error.message },
-    //  ];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((res) => (
+      {
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : res.reason,
+      }
+    )));
 }
