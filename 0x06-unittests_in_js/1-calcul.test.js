87479745b1c362a041a1f64817ac229c17cbe38f
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-09-20 18:10:00 +0000

    task 1

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', -1.5, -3.7), -5);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the subtraction of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.7, 3.7), -2);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1.7, -3.7), 2);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      assert.strictEqual(calculateNumber('DIVIDE', 4.6, 1.3), 5);
+    });
+
+    it('should return "Error" if dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
