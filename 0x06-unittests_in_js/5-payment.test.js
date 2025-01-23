c0073d37df6fb54cdd803109af61a0c29cda1a20
Mercy <ndirangumercy4@gmail.com>
2025-01-23 10:48:25 +0000

    task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log the correct total when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('should log the correct total when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
