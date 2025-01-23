40efb3bb744c988ba3daa7a8679a7e53c6f2b73e
Mercy <ndirangumercy4@gmail.com>
2025-01-23 10:34:33 +0000

    task 4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10)
+
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with correct arguments and log the total', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
