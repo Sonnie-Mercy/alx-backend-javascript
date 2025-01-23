3955e2220fe819026b4f478f540832d3c633eb9f
Mercy <ndirangumercy4@gmail.com>
2025-01-23 10:52:36 +0000

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a successful response when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.have.property('data').that.equals('Successful response from the API');
+        done();
+      })
+      .catch(done);
+  });
+});
